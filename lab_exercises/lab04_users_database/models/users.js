b55c8f8bf16e3364feeafda2e5aa808fa4b18043
sahilBirmaan <jerry014a@gmail.com>
2021-02-17 15:01:34 -0500

    changed userSchema and defined it according to given json file and inserted given users json records

--- lab_exercises/lab04_users_database/models/users.js
+++ lab_exercises/lab04_users_database/models/users.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
 const usersSchema = new mongoose.Schema({
+    name: {type: String},
+
     username: {
       type: String,
       required: true,
@@ -19,39 +21,34 @@ const usersSchema = new mongoose.Schema({
       }
     },
 
-    city: {
-      type: String,
-      required: true,
-      validate : {
-        validator: function(v) {
-          let cityRegex = /^[a-zA-Z ]*$/;
-          return cityRegex.test(v);
-        },
-        message: 'City validation failed. Please ONLY use alphabets and space in the city name'
-      }
-    },
-
-    web: {
-      type: String,
-      required: true,
-      validate : {
-        validator: function(v) {
-          let webRegex = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/;
-          return webRegex.test(v);
-        },
-        message: 'Web/URL address validation failed. Please make sure that the provided url is valid'
-      }
-    },
-
-    zip: {
-      type: String,
-      required: true,
-      validate : {
-        validator: function(v) {
-          let zipRegex = /^\d{5}(?:[- ]\d{4})?$/;
-          return zipRegex.test(v);
-        },
-        message: 'Zip code validation failed. Please provide a correct zip code, in a valid format'
+    address: {
+      street: { type: String },
+      suite: { type: String },
+      city: {
+        type: String,
+        required: true,
+        validate : {
+          validator: function(v) {
+            let cityRegex = /^[a-zA-Z ]*$/;
+            return cityRegex.test(v);
+          },
+          message: 'City validation failed. Please ONLY use alphabets and space in the city name'
+        }
+      },
+      zipcode: {
+        type: String,
+        required: true,
+        validate : {
+          validator: function(v) {
+            let zipRegex = /^\d{5}(?:[- ]\d{4})?$/;
+            return zipRegex.test(v);
+          },
+          message: 'Zip code validation failed. Please provide a correct zip code, in a valid format'
+        }
+      },
+      geo: {
+        lat: { type: Number },
+        lng: { type: Number }
       }
     },
 
@@ -65,6 +62,24 @@ const usersSchema = new mongoose.Schema({
         },
         message: 'Phone number validation failed. Please make sure that provided phone number is correct and is in the right format'
       }
+    },
+
+    website: {
+      type: String,
+      required: true,
+      validate : {
+        validator: function(v) {
+          let webRegex = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/;
+          return webRegex.test(v);
+        },
+        message: 'Web/URL address validation failed. Please make sure that the provided url is valid'
+      }
+    },
+
+    company: {
+      name: { type: String },
+      catchPhrase: { type: String },
+      bs: { type: String }
     }
 });
 
